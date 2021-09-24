0f00ad0d8546499f8bdba256d52fb5a80559a33f
Stef Ruinard <stefruinard@hotmail.com>
2021-09-24 16:45:22 +0200

    Deployment succeeded

--- infra.sh
+++ infra.sh
@@ -23,4 +23,6 @@ az appservice plan create --name $WEBAPP_PLAN --sku B1 --is-linux
 az webapp create --name $WEBAPP  --plan $WEBAPP_PLAN --runtime 'python|3.7'
 az webapp cors add  -n $WEBAPP --allowed-origins '*'
 az webapp config set -n $WEBAPP --startup-file 'startup.sh'
+az webapp config appsettings set -n $WEBAPP --settings "SCM_DO_BUILD_DURING_DEPLOYMENT=1"
 
+az webapp up -n $WEBAPP
