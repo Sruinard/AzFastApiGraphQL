07d478e8c09e07161064272a336ab5327860e7c2
Stef Ruinard <stefruinard@hotmail.com>
2021-09-24 14:42:55 +0200

    test deployment

---
+++ infra.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# DECLARE
+## general
+RESOURCE_GROUP="raboapim"
+LOCATION="westeurope"
+
+
+## webapp
+WEBAPP_PLAN="graphql-azure-${RANDOM}"
+WEBAPP="graphql-azure-${RANDOM}"
+
+#BUILD
+# Configure defaults values
+az configure --scope local --defaults group=$RESOURCE_GROUP location=$LOCATION 
+
+# create resource group
+az group create --resource-group $RESOURCE_GROUP -l $LOCATION
+echo "Resource group created"
+
+# create static webapp
+az appservice plan create --name $WEBAPP_PLAN --sku B1 --is-linux
+az webapp create --name $WEBAPP  --plan $WEBAPP_PLAN --runtime 'python|3.6'
+az webapp cors add  -n $WEBAPP --allowed-origins '*'
+az webapp config set -n $WEBAPP --startup-file 'gunicorn -w 2 -k uvicorn.workers.UvicornWorker payments.app:app'
+
+az webapp up -n $WEBAPP
--- payments/app.py
+++ payments/app.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import graphene
 from starlette.graphql import GraphQLApp
 from payments.schema import Query, Mutation
+import uvicorn
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_route("/creditcard", GraphQLApp(schema=graphene.Schema(query=Query, mutation=Mutation)))
 
 @app.get("/")
 def homepage():
     return "Fast API + GraphQL"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
