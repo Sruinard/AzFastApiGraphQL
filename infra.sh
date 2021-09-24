c589b998371e36dbe5d0f1d31411e9e9840a1f35
Stef Ruinard <stefruinard@hotmail.com>
2021-09-24 15:14:30 +0200

    add init

--- infra.sh
+++ infra.sh
@@ -20,7 +20,7 @@ echo "Resource group created"
 
 # create static webapp
 az appservice plan create --name $WEBAPP_PLAN --sku B1 --is-linux
-az webapp create --name $WEBAPP  --plan $WEBAPP_PLAN --runtime 'python|3.6'
+az webapp create --name $WEBAPP  --plan $WEBAPP_PLAN --runtime 'python|3.7'
 az webapp cors add  -n $WEBAPP --allowed-origins '*'
 az webapp config set -n $WEBAPP --startup-file 'gunicorn -w 2 -k uvicorn.workers.UvicornWorker payments.app:app'
 
