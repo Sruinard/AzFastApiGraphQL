23744a162f33c3256d0f3f386d006340c001f361
Stef Ruinard <stefruinard@hotmail.com>
2021-10-14 07:32:41 +0200

    Ready for deployment.

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from functools import lru_cache
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-	POSTGRESDB_URL: str
-	IS_DEBUG: bool
-
-	class Config:
-		env_file = ".env"
+        POSTGRESDB_URL: str
+        IS_DEBUG: bool
 
 @lru_cache()
 def load_config():
-	return Settings()
+        return Settings(
+                POSTGRESDB_URL=os.environ.get("POSTGRESDB_URL", "sqlite:///develop.db"),
+                IS_DEBUG=os.environ.get("IS_DEBUG", False)
+        )
